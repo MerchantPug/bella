e0097535bbe555a69bec819094033d2e8229357f
Pug <batterypug@gmail.com>
2022-08-21 23:55:08 +1000

    Remove unnecessary client annotations

--- src/main/java/com/github/merchantpug/bella/BellRenderModifierRegistryImpl.java
+++ src/main/java/com/github/merchantpug/bella/BellRenderModifierRegistryImpl.java
@@ -5,7 +5,6 @@ import com.github.merchantpug.bella.api.BellRenderModifiers;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
 public class BellRenderModifierRegistryImpl implements BellRenderModifierRegistry {
 	public void registerBellRenderModifiers(BellRenderModifiers modifiers) {
 		Bella.BELL_RENDER_MODIFIERS.add(modifiers);
--- src/main/java/com/github/merchantpug/bella/api/BellRenderModifierRegistry.java
+++ src/main/java/com/github/merchantpug/bella/api/BellRenderModifierRegistry.java
@@ -1,9 +1,5 @@
 package com.github.merchantpug.bella.api;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
 public interface BellRenderModifierRegistry {
 	void registerBellRenderModifiers(BellRenderModifiers modifiers);
 }
